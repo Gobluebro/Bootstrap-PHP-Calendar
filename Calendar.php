eb48f59942a5a86386c12e5b7f7f01ea46dfc4b9
Griffin Strayer <griffinstrayer+coding@gmail.com>
2017-11-01 04:07:58 -0400

    Running with Database
    It is currently working with a local database. Used a bit more SQL to get the data I want in the correct order instead of using PHP to do it. Had to install xampp to get a local test in. I thought you could just run php in an html file and test it with a browser. I guess not. It runs in the CalendarPage HTML file now.

--- Calendar.php
+++ Calendar.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+$username = "fake";
+$password = "fakepassword";
+$dbname = "faketest";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -10,39 +10,36 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
-$sql = "SELECT event, dateofevent, eventdata, image, title FROM test";
+// select the events, make sure the date is always in the same format, eventdata, image url, title, from the test table where they are starting 3 days from the current time and beyeond
+$sql = "SELECT event, DATE_FORMAT(dateofevent,'%Y-%m-%d') AS formatted_date, eventdata, image, title FROM test_table WHERE dateofevent >= NOW() - INTERVAL 3 DAY ORDER BY dateofevent ASC";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-// output data of each row
-// need variable for class and ids
-$test = 0;
-// 3 days before the current date
-$expirydate = (new DateTime($expirydate))->sub(new DateInterval('P3D'))->format('Y-m-d');
-?>
-<div id="accordion" role="tablist"> 
-<?php while($row = $result->fetch_assoc()) { 
-    // only show the events >= 3 days 
-    if ($row["dateofevent"] < $expirydate){
+    // output data of each row
+    // need variable for class and ids
+    $test = 0;
+    ?>
+    <div id="accordion" role="tablist"> 
+    <?php while($row = $result->fetch_assoc()) { 
         ?>
-        <div class="card">
-            <div class="card-header" role="tab" id="heading_<?php echo $test; ?>">
-                <h5 class="mb-0">
-                    <a data-toggle="collapse" href="#collapse_<?php echo $test; ?>" aria-expanded="false" aria-controls="collapse_<?php echo $test; ?>">
-                        <?php echo $row["event"]. " - " . $row["title"]. " - " . $row["dateofevent"]; ?>
-                    </a>
-                </h5>
-            </div>
-            <div id="collapse_<?php echo $test; ?>" class="collapse" role="tabpanel" aria-labelledby="heading_<?php echo $test; ?>" data-parent="#accordion">
-                <div class="card-body">
-                    <?php echo $row["eventdata"]; ?>
+            <div class="card">
+                <div class="card-header" role="tab" id="heading_<?php echo $test; ?>">
+                    <h5 class="mb-0">
+                        <a data-toggle="collapse" href="#collapse_<?php echo $test; ?>" aria-expanded="false" aria-controls="collapse_<?php echo $test; ?>">
+                            <?php echo $row["event"]. " - " . $row["title"]. " - " . $row["formatted_date"] ?>
+                        </a>
+                    </h5>
+                </div>
+                <div id="collapse_<?php echo $test; ?>" class="collapse" role="tabpanel" aria-labelledby="heading_<?php echo $test; ?>" data-parent="#accordion">
+                    <div class="card-body">
+                        <?php echo $row["eventdata"]; ?>
+                        <img src="<?php echo $row["image"]; ?>" />
+                    </div>
                 </div>
             </div>
-        </div>"
-    <?php 
+        <?php 
+        $test++;
     }
-    $test++;
 } else {
     echo "0 results";
 }
